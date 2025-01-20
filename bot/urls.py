aa36b6249af49f3118fd9deae26a68b07f57aa3e
PenDatabase <davideneasatochibueze@gmail.com>
2025-01-21 00:54:40 +0100

    modified telegram url link

--- bot/urls.py
+++ bot/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import RedirectView
 from .views import paystack_callback, get_order_details, home
 
 urlpatterns = [
-    path('telegram_bot/', RedirectView.as_view(url="https://t.me/indomie_order_bot/", permanent=True), name="telegram_url"),
+    path('telegram_bot/', RedirectView.as_view(url="https://t.me/cu_indomie_guy_bot", permanent=True), name="telegram_url"),
     path('', home, name="home"),
     path('paystack/callback/', paystack_callback, name='paystack_callback'),
     path("api/orders/<int:order_id>/", get_order_details, name="order-details"),
